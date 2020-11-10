e2db4d747e69d9d44a4760941d08e1eefc97730e
Lucxtor <37386914+Lucxtor@users.noreply.github.com>
2020-11-10 11:04:52 -0300

    Update questoesEco.js

--- questoesEco.js
+++ questoesEco.js
@@ -22,7 +22,7 @@ questoesEco = [
             "concordo": "Você faz um controle gradativo sobre os preços para que mantenham-se mais baixos e passem por uma transição mais suave a fim de auxiliar os consumidores do seu país",
             "neutro": "Opta por repassar essa decisão ao ministro da economia afinal você não sabe a melhor decisão ou não quer se posicionar referente a um ponto como esse",
             "discordo": "Você pretende deixar o mercado nacional variar junto ao internacional a não ser que isso chegue a pontos extremos",
-            "discordoFortemente": "Não fazer nada e deixar que os preços variem junto com o mercado internacional "
+            "discordoFortemente": "Não fazer nada e deixar que os preços variem junto com o mercado internacional"
         },
         "effect": {
             "econ": 10,
@@ -37,7 +37,7 @@ questoesEco = [
             "concordoFortemente": "Efetuar a venda integral dos recursos hidricos a fim de aumentar a liberdade pessoal pelo livre mercado",
             "concordo": "Efetuar a venda parcial dos recursos hidricos a fim de aumentar a liberdade pessoal pelo livre mercado",
             "neutro": "Prefere observar casos semelhantes em países vizinhos para entender como a venda pode afetar as populações de baixa renda, está aberto à negociações.",
-            "discordo": "Manter a oferta em aberto mas não fecha com a empresa pois você considera que a interferencia do livre mercado não vai ser benéfica para a população ",
+            "discordo": "Manter a oferta em aberto mas não fecha com a empresa pois você considera que a interferencia do livre mercado não vai ser benéfica para a população",
             "discordoFortemente": "Recusar a oferta para que a população se mantenha livre uma vez que o estado é o maior proporcionador de liberdade para os cidadãos"
         },
         "effect": {
@@ -50,11 +50,11 @@ questoesEco = [
     {
         "question": "Um dos problemas que podem ser facilmente encontrados são crises economicas inevitaveis durante seu mandato, logo, como pretende trabalhar com a parte social nesse periodo",
         "respostas": {
-            "concordoFortemente": "Em momento de crise economica os servições sociais serão os primeiros a serem cortados pelo bem da economia",
+            "concordoFortemente": "Em momento de crise economica os serviços sociais serão os primeiros a serem cortados pelo bem da economia",
             "concordo": "Em momentos de crise o social deve ser suspenso por periodo indeterminado para manter o orçamento em ordem",
             "neutro": "Não tenho um real posicionamento ou depende exclusivamente da ocasião",
             "discordo": "Em momentos de crise o social deve ser mantido para o bem estar da população",
-            "discordoFortemente": "Em momento de crise economica os servições sociais terão mais ênfase pois é importante proteger os cidadãos"
+            "discordoFortemente": "Em momento de crise economica os serviços sociais terão mais ênfase pois é importante proteger os cidadãos"
         },
         "effect": {
             "econ": -10,
@@ -67,7 +67,7 @@ questoesEco = [
         "question": "O fator mais importante para o progresso é a pesquisa, podendo elas serem publicamente financiadas ou pertencerem ao setor privado, mas qual é mais benefica ao progresso?",
         "respostas": {
             "concordoFortemente": "Pesquisas publicamente financiadas são um maior benefício à população do que deixá-las exclusivamente sob a influência (Lobby) do setor privado.",
-            "concordo": "Pesquisas publicamente financias em geral são mais beneficas que as demais",
+            "concordo": "Pesquisas publicamente financiadas em geral são mais beneficas que as demais",
             "neutro": "Não tenho um real posicionamento ou depende exclusivamente da ocasião",
             "discordo": "Pesquisas do setor privado em geral são mais beneficas que as demais",
             "discordoFortemente": "Pesquisas sob a administração do setor privado são um maior benefício à população do que deixá-las exclusivamente para o setor publico."
@@ -83,7 +83,7 @@ questoesEco = [
         "question": "Durante sua gestão você analisa uma demanda muito grande por importação de produtos por parte da população.",
         "respostas": {
             "concordoFortemente": "Você vê essa demanda e como acha benéfico tenta influenciar as pessoas a realmente fazerem uso do comércio internacional e busca por melhorar as leis a fim de facilitar a importação.",
-            "concordo": "Você vê essa demanda e como acha benéfico tenta influenciar as pessoas a realmente fazerem uso do comércio internacional",
+            "concordo": "Você vê essa demanda e como acha benéfico, tenta influenciar as pessoas a realmente fazerem uso do comércio internacional",
             "neutro": "Você repassa a decisão ao ministro da Indústria, Comércio Exterior e Serviços ou simplesmente deixa esse fato pois não é tão significativo para o governo",
             "discordo": "Você opta por aumentar as taxas de importação mas ainda deixar a livre escolha da população o que prefere",
             "discordoFortemente": "após essa análise você opta por aumentar as taxas de importação e restringir diversos produtos de serem exportados a fim de influenciar o comércio nacional"
@@ -112,7 +112,7 @@ questoesEco = [
         }
     },
     {
-        "question": "Antes de tudo vamos delimitar a existencia ou não dos mesmos  ",
+        "question": "Sobre serviços sociais, vamos delimitar a existencia ou não dos mesmos",
         "respostas": {
             "concordoFortemente": "Em geral os serviços sociais não são beneficos e devem ser abolidos em favor da caridade privada",
             "concordo": "Em sua maioria os seriços sociais não são beneficos e devemos dar mais espaço para a caridade privada",
@@ -144,13 +144,13 @@ questoesEco = [
         }
     },
     {
-        "question": "Com o passar do tempo nascem novas gerações e com elas nascem também uma nova decisão a ser tomada, quando uma pessoa venha a falecer ela deixa seus pertences a um sucessor, esses pertences são conhecidos como herança, na sua sociedade a herança é",
+        "question": "Com o passar do tempo nascem novas gerações e com elas nascem também uma nova decisão a ser tomada, quando uma pessoa vem a a falecer ela deixa seus pertences a um sucessor, esses pertences são conhecidos como herança, na sua sociedade a herança é",
         "respostas": {
             "concordoFortemente": "uma forma legítima de riqueza",
             "concordo": "é uma forma legítima de riqueza porém a mesma deve ser comprovada com pelo menos grau de parentesco",
-            "neutro": "",
-            "discordo": "não é uma forma valida a não ser que seja um parente direto por exemplo pai para filho",
-            "discordoFortemente": "não é valida e a origem de qualquer forma de riqueza deve ser comprovada"
+            "neutro": "Não tenho um real posicionamento ou depende exclusivamente da ocasião",
+            "discordo": "não é uma forma válida a não ser que seja um parente direto por exemplo pai para filho",
+            "discordoFortemente": "não é válida e a origem de qualquer forma de riqueza deve ser comprovada"
         },
         "effect": {
             "econ": -10,
@@ -176,11 +176,11 @@ questoesEco = [
         }
     },
     {
-        "question": "Mas este foi apenas um dos casos, outros muitos acontecerão então que grau de interferencia é aceitavel para não comprometer a economia?",
+        "question": "O problema encontrado com o aumento do preço do Petróleo foi apenas um dos casos, outros muitos acontecerão, então, que grau de interferencia é aceitavel para não comprometer a economia?",
         "respostas": {
             "concordoFortemente": "Uma intervenção excessiva do Estado no Mercado é um risco à economia.",
             "concordo": "Uma intervenção excessiva pode apresentar um risco",
-            "neutro": "",
+            "neutro": "Não tenho um real posicionamento ou depende exclusivamente da ocasião",
             "discordo": "Uma intervenção moderada não é necessariamente um problema",
             "discordoFortemente": "Uma intervenção por parte do Estado não apresenta nenhum risco"
         },
@@ -192,13 +192,13 @@ questoesEco = [
         }
     },
     {
-        "question": "Saude é um dos serviços essenciais para a população mas a questão é, como esse é distribuida",
+        "question": "Saúde é um dos serviços essenciais para a população, mas a questão é como ela deve ser oferecida",
         "respostas": {
-            "concordoFortemente": "A saude é um serviço essencial mas aqueles que podem pagar devem receber o melhor acsso a saude",
-            "concordo": "A saude tem sua importancia mas seu acesso deve ser priorizado a pessoas capazes de pagar pelo serviço",
+            "concordoFortemente": "A saúde é um serviço essencial, mas aqueles que podem pagar devem receber o melhor acesso à saúde",
+            "concordo": "A saúde tem sua importância, mas seu acesso deve ser priorizado à pessoas capazes de pagar pelo serviço",
             "neutro": "Não tenho certeza ou sou indiferente",
-            "discordo": "A saude como ponto importantissimo deve ser ampla para toda a população mas não precisa ser de acesso facilitado",
-            "discordoFortemente": "A area da saude devido sua importancia deve ser um direito igual para todos e de facil acesso"
+            "discordo": "A saúde como ponto importantíssimo deve ser ampla para toda a população, mas não precisa ser de acesso facilitado",
+            "discordoFortemente": "A área da saúde, devido à sua importância, deve ser um direito igual para todos e de facil acesso"
         },
         "effect": {
             "econ": -10,
@@ -208,9 +208,9 @@ questoesEco = [
         }
     },
     {
-        "question": "Educação é o serviço mais importante pro desenvolvimento da sociedade logo ela deve",
+        "question": "Educação é o serviço mais importante pro desenvolvimento da sociedade, logo ela deve",
         "respostas": {
-            "concordoFortemente": "Ser de qualidade e de facil acesso a todos os cidadãos",
+            "concordoFortemente": "Ser de qualidade e de fácil acesso a todos os cidadãos",
             "concordo": "Ser de qualidade para boa parte da população",
             "neutro": "Não vejo tanta importancia nesse tópico ou não tenho certeza",
             "discordo": "Ser um serviço pouco amplo para acesso de apenas alguns cidadãos",
@@ -240,12 +240,12 @@ questoesEco = [
         }
     },
     {
-        "question": "É comum muitas nações ao redor do mundo  criar ligas regionais geralmente com fins economicos, o que você acha destes sistemas?",
+        "question": "É comum muitas nações ao redor do mundo criarem ligas regionais ou blocos econômicos, o que você acha destes sistemas?",
         "respostas": {
             "concordoFortemente": "Eu apoio ligas regionais de países, considero que como a União Europeia ou o Mercosul trazem muitas vantagens para a economia do país.",
             "concordo": "Eu sou parcialmente a favor das ligas regionais de países, considero que tais ligas podem trazem vantagens para a economia do país sendo favoravel a economia.",
-            "neutro": "",
-            "discordo": "Eu sou parcialmente contra ligas regionais de países, considero que tais ligas não trazem vantagens para a economia do país podendo ser desfavoravel a economia.",
+            "neutro": "Não tenho um real posicionamento ou depende exclusivamente da ocasião",
+            "discordo": "Eu sou parcialmente contra ligas regionais de países, considero que tais ligas não trazem vantagens para a economia do país podendo ser desfavoravel a economia..",
             "discordoFortemente": "Eu sou contra ligas regionais de países, considero que tais ligas trazem muitas desvantagens para a economia do país como um desequilibrio da balança comercial e enfraquecimento do mercado interno."
         },
         "effect": {
@@ -256,10 +256,10 @@ questoesEco = [
         }
     },
     {
-        "question": "Assim como nosso pais passa por algumas dificuldades nenhum dos demais está livre das mesmas, muitas vezes vamos precensiar paises necessitado, a questão é, como lidar com isso?",
+        "question": "Assim como nosso pais passa por algumas dificuldades, nenhum dos demais está livre das mesmas, muitas vezes vamos presenciar paises em dificuldades, a questão é, como lidar com isso?",
         "respostas": {
             "concordoFortemente": "O apoio financeiro aos países necessitados é um desperdício de dinheiro.",
-            "concordo": "O apio aos demais paises não é valido a não ser que nos de algum tipo de vantagem",
+            "concordo": "O apoio aos demais paises não é valido a não ser que nos de algum tipo de vantagem",
             "neutro": "não quero me posicionar ou não tenho certeza sobre o assunto",
             "discordo": "O apoio financeiro é uma boa forma de auxiliar um pais necessitado, sendo o certo a se fazer e podendo influenciar em uma futura aliança",
             "discordoFortemente": "É essencial dar apoio financeiro a essses paises sempre que pudermos"
@@ -272,11 +272,11 @@ questoesEco = [
         }
     },
     {
-        "question": "Como exemplo de outras nações você analisa um possivel problema na parte ambiental, por fim, você decide",
+        "question": "Como exemplo de outras nações, você analisa um possivel problema na parte ambiental, por fim, você decide",
         "respostas": {
-            "concordoFortemente": "Criar diversas regulamentações para proteger o meio ambiente, considerando que essas são essenciais para o futuro das proximas gerações ",
-            "concordo": "Cria regulamentações ambientais basicas para assegurar o meio ambiente e poder melhorar um pouco o futura das proximas gerações ",
-            "neutro": "",
+            "concordoFortemente": "Criar diversas regulamentações para proteger o meio ambiente, considerando que essas são essenciais para o futuro das proximas gerações",
+            "concordo": "Cria regulamentações ambientais basicas para assegurar o meio ambiente e poder melhorar um pouco o futura das proximas gerações",
+            "neutro": "Não tenho um real posicionamento ou depende exclusivamente da ocasião",
             "discordo": "Criar poucas regulamentações, apenas para assegurar parte do meio ambiente, já que as regulamentações não tem tanta importancia",
             "discordoFortemente": "Deixar as pessoas livres e acredita que o meio ambiente poderá se manter já que as regulações diminuem a liberdade e não ajudam em nada"
         },
@@ -288,13 +288,13 @@ questoesEco = [
         }
     },
     {
-        "question": "Os impostos são importantes para a manutenção do estado e atualmente existem diversas religioes emergentes nas sociedades, até então em algumas sociedades como a brasileira, as igrejas possuem uma imunidade tributaria e em outras como a Austria as pessoas declaram sua fé ao estado e ele repassa os impostos religiosos cobrados da população e repassa para as respectivas religioes",
+        "question": "Os impostos são importantes para a manutenção do estado e atualmente existem diversas religiões emergentes nas sociedades, até então, em algumas sociedades como a brasileira, as igrejas possuem uma imunidade tributária e em outras como a Áustria as pessoas declaram sua fé ao estado e ele repassa os impostos para as respectivas religioes",
         "respostas": {
-            "concordoFortemente": "As igrajas devem contribuir com a manutenção do poder e pagar taxas tributarias assim como as demais instituições",
-            "concordo": "É importanteque as igrejas contribuam mas não serão gerados impostos como os impostos empresariais para esses estabelecimentos ",
-            "neutro": "",
+            "concordoFortemente": "As igrejas devem contribuir com a manutenção do poder e pagar taxas tributarias assim como as demais instituições",
+            "concordo": "É importanteque as igrejas contribuam, mas não serão gerados impostos como os impostos empresariais para esses estabelecimentos",
+            "neutro": "Não tenho um real posicionamento ou depende exclusivamente da ocasião",
             "discordo": "As igrejas permaneceram livres de impostos mas serão incentivadas a contribuir com a manutenção do estado",
-            "discordoFortemente": "A imunidade tributaria das igrejas será mantida em minha sociedade com a intenção de não interferir na fé das pessoas "
+            "discordoFortemente": "A imunidade tributaria das igrejas será mantida em minha sociedade com a intenção de não interferir na fé das pessoas"
         },
         "effect": {
             "econ": 5,
@@ -304,13 +304,13 @@ questoesEco = [
         }
     },
     {
-        "question": "Mais uma vez resssaltando a importancia da area da saude o que você considera o ideal",
+        "question": "Mais uma vez resssaltando a importância da área da saúde, o que você considera o ideal",
         "respostas": {
-            "concordoFortemente": "A saude deveria ser um sistema unico e universal para todos",
-            "concordo": "A saude deve ser um sistema unico e abrangente a varios paises",
+            "concordoFortemente": "A saúde deveria ser um sistema unico e universal para todos",
+            "concordo": "A saúde deve ser um sistema unico e abrangente a varios paises",
             "neutro": "Não tenho certeza ou considero indiferente",
-            "discordo": "A saude deve ser individual ou pertencente a ligas regionais com variados sistemas",
-            "discordoFortemente": "A saude deve ser individual de cada nação e com variados sistemas"
+            "discordo": "A saúde deve ser individual ou pertencente a ligas regionais com variados sistemas",
+            "discordoFortemente": "A saúde deve ser individual de cada nação e com variados sistemas"
         },
         "effect": {
             "econ": 10,
@@ -336,11 +336,11 @@ questoesEco = [
         }
     },
     {
-        "question": "Apesar de que você tomara todas as decisões, nesse momento é importante sabermos como elas seriam tratadas na realidade, por mais que você tenha sido eleito. Como a sua liderança pretende tratar de interesses ideológicos e possiveis conflitos que venham a existir? ",
+        "question": "Apesar de que você tomara todas as decisões, nesse momento é importante sabermos como elas seriam tratadas na realidade, por mais que você tenha sido eleito. Como a sua liderança pretende tratar de interesses ideológicos e possiveis conflitos que venham a existir?",
         "respostas": {
             "concordoFortemente": "É importante que coloquemos os interesses do nosso grupo ideológico acima de todos os demais justificado pela minha eleição ao cargo.",
             "concordo": "É importante que coloquemos os interesses do nosso grupo ideológico acima de todos mas ainda assim deve existir uma consulta aos demais para que a solução não fuja da realidade.",
-            "neutro": "Contanto que pague os impostos e não traga nenhum prejuízo ao país, os grupos ideológicos podem se manifestar, desde que de  maneira respeitosa ",
+            "neutro": "Contanto que pague os impostos e não traga nenhum prejuízo ao país, os grupos ideológicos podem se manifestar, desde que de  maneira respeitosa",
             "discordo": "É importante que coloquemos os interesses dos grupos ideológicos lado a lado para que possam ser analisadoscom pesos diversos para cada ideologia.",
             "discordoFortemente": "É importante que coloquemos os interesses dos grupos ideológicos lado a lado, pois apesar de ser eleito pela minha ideologia todas devem ser consultadas a fim de obter a melhor resposta."
         },
